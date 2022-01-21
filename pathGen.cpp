b0bd18f10f3b74bc2dd05b09b3e5618402b7853f
DylanLeersen <dylanchaseleersen@gmail.com>
2022-01-21 12:03:10 +1100

    made it so anything that isn't the path, ending or start will be turned to #s

--- pathGen.cpp
+++ pathGen.cpp
@@ -398,6 +398,12 @@ void genMaze(){
         
     }
 
-    
+    for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+            if(maze[i][j] != ' ' && maze[i][j] != 'E' && maze[i][j] != '0'){
+                maze[i][j] = '#';
+            }
+        }
+    }
     printMaze();
 }
